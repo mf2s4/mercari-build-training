e75cdc41eed19e35d6a0cd9a9cf9662acc7b5490
Em <mgfe2sio4.7@gmail.com>
2024-02-24 22:04:46 +0000

    Debugged word pattern

--- extra/extra1/word_pattern.py
+++ extra/extra1/word_pattern.py
@@ -1,11 +1,14 @@
-def findPattern(p, s):
-    s = s.split(" ")
-    if len(p) != len(s):
-        return False
-    pattern = {}
-    for i in range(len(p)):
-        if p[i] not in pattern:
-            pattern[p[i]] = s[i]
-        elif pattern[p[i]] != s[i]:
+def wordPattern(pattern: str, s: str) -> bool:
+        words = list(s.split(" "))
+        hash_map = {}
+        if len(pattern) != len(words): 
             return False
-    return True
+
+        for i in range(len(words)):
+            if pattern[i] not in hash_map:
+                if words[i] in hash_map.values():
+                    return False
+                hash_map[pattern[i]] = words[i]
+            elif pattern[i] in hash_map and hash_map[pattern[i]] != words[i]:
+                return False
+        return True
